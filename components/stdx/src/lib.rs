baafc5045cdbb52d219c83446774a814ff0d6608
f01dab1e <andriy.n@obox.systems>
2023-07-03 13:12:33 +0000

    move `find_rust_code_blocks` function to markdown module

--- components/stdx/src/lib.rs
+++ components/stdx/src/lib.rs
@@ -5,6 +5,8 @@
 
 mod ir;
 mod macros;
+/// This module provides functionality for parsing and rendering Markdown.
+pub mod markdown;
 mod traits;
 
 pub use ir::{CommandBuilder, Property};
@@ -19,54 +21,4 @@ pub type Result<T = (), E = miette::Report> = miette::Result<T, E>;
 /// returns it for further configuration of the CLI.
 pub fn cli<T>(state: T) -> CommandBuilder<T, 0> {
     CommandBuilder::with_state(state)
-}
-
-/// Finds rust code blocks in the given markdown text.
-pub fn find_rust_code_blocks(text: &str) -> Vec<Box<str>> {
-    lazy_regex::lazy_regex!(r"(?s)```rust\s*\n(.*?)\n\s*```")
-        .captures_iter(text)
-        .map(|item| item[1].into())
-        .collect()
-}
-
-#[cfg(test)]
-mod tests {
-    use expect_test::expect;
-    use itertools::Itertools as _;
-
-    #[test]
-    fn find_rust_code_blocks() {
-        let markdown = r#"
-```rust
-fn main() {
-    println!("Hello, world!");
-}
-```
-
-```
-
-```
-
-```zig
-const std = @import("std");
-
-pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("Hello, {s}!\n", .{"world"});
-}
-```
-"#;
-
-        let blocks = super::find_rust_code_blocks(markdown)
-            .into_iter()
-            .enumerate()
-            .map(|(index, text)| lazy_format::lazy_format!("{index}: {text}"))
-            .join("\n");
-
-        expect![[r#"
-            0: fn main() {
-                println!("Hello, world!");
-            }"#]]
-        .assert_eq(&blocks);
-    }
 }
